e8b3ec960d2b90fe57fb911fa8be500482e1cf5a
Samuel Gélineau <gelisam+github@gmail.com>
2017-11-24 09:11:28 -0500

    Seq instead of list

--- linear-watertight/src/Obj.hs
+++ linear-watertight/src/Obj.hs
@@ -1,7 +1,11 @@
 module Obj where
 
 import Data.Foldable
+import Data.Sequence (Seq)
 import Text.Printf
+
+-- $setup
+-- >>> import qualified Data.Sequence as Seq
 
 
 -- the standard .obj format for 3D meshes,
@@ -9,13 +13,13 @@ import Text.Printf
 type Vertex = (Float, Float, Float)
 type Face = [Int]
 data Obj = Obj
-  { objVertices :: [Vertex]
-  , objFaces    :: [Face]
+  { objVertices :: Seq Vertex
+  , objFaces    :: Seq Face
   }
   deriving Show
 
 -- |
--- >>> printObj $ Obj [(1,2,3),(4,5,6),(7,8,9)] [[1,2,3]]
+-- >>> printObj $ Obj (Seq.fromList [(1,2,3),(4,5,6),(7,8,9)]) (Seq.fromList [[1,2,3]])
 -- v 1.0 2.0 3.0
 -- v 4.0 5.0 6.0
 -- v 7.0 8.0 9.0
