a851f4dfaca9b00081e7bfc94a9b2a35d25ef867
Samuel Gélineau <gelisam+github@gmail.com>
2017-11-24 09:59:59 -0500

    only points

--- linear-watertight/src/LinearWatertight.hs
+++ linear-watertight/src/LinearWatertight.hs
@@ -1,6 +1,44 @@
-module LinearWatertight where
+{-# LANGUAGE GeneralizedNewtypeDeriving, GADTs #-}
+{-# OPTIONS -Wno-unused-top-binds #-}
+module LinearWatertight
+  ( ModelBuilding
+  , Point, addPoint
+  , Watertight3dModel, makeWatertight3dModel, renderWatertight3dModel
+  , runTests
+  ) where
 
+import Control.Monad.Trans.State
+import Data.Monoid
 import Test.DocTest
+import qualified Data.Sequence as Seq
+
+import Obj
+
+
+newtype ModelBuilding a = PrivateModelBuilding
+  { unModelBuilding :: State Obj a }
+  deriving (Functor, Applicative, Monad)
+
+newtype Point = PrivatePoint
+  { unPoint :: Int }
+  deriving Show
+
+newtype Watertight3dModel = PrivateWatertight3dModel
+  { unWatertight3dModel :: Obj }
+  deriving Show
+
+addPoint :: Vertex -> ModelBuilding Point
+addPoint v = PrivateModelBuilding $ do
+  modify $ \obj -> obj { objVertices = objVertices obj <> Seq.singleton v }
+  PrivatePoint . length . objVertices <$> get
+
+makeWatertight3dModel :: ModelBuilding () -> Watertight3dModel
+makeWatertight3dModel = PrivateWatertight3dModel
+                      . flip execState (Obj mempty mempty)
+                      . unModelBuilding
+
+renderWatertight3dModel :: Watertight3dModel -> Obj
+renderWatertight3dModel = unWatertight3dModel
 
 
 -- |
@@ -16,6 +54,17 @@ import Test.DocTest
 -- ...
 -- ...error...
 -- ...
+--
+-- >>> :{
+-- printObj . renderWatertight3dModel . makeWatertight3dModel $ do
+--   _ <- addPoint (1,2,3)
+--   _ <- addPoint (4,5,6)
+--   _ <- addPoint (7,8,9)
+--   pure ()
+-- :}
+-- v 1.0 2.0 3.0
+-- v 4.0 5.0 6.0
+-- v 7.0 8.0 9.0
 runTests :: IO ()
 runTests = doctest [ "linear-watertight/src/LinearWatertight.hs"
                    , "linear-watertight/src/Obj.hs"
